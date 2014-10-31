a9623f190fc2740f5d73f52aedb73a8420b68464
Daniel <dbecker.fl@gmail.com>
2014-10-31 12:14:41 -0400

    Narrow down club member approved hours

--- src/Maclay/ServiceBundle/Controller/ClubController.php
+++ src/Maclay/ServiceBundle/Controller/ClubController.php
@@ -7,13 +7,49 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Maclay\ServiceBundle\Entity\Record;
 use Maclay\ServiceBundle\Form\ClubRecordType;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ClubController extends Controller
 {
-    public function manageClubAction(){
+    private $schoolStart;
+    private $q1End;
+    private $q2End;
+    private $q3End;
+    private $q4End;
+    
+    public function manageClubAction($quarter = 0, $export = false){
         $user = $this->getUser();
         
         $clubs = $user->getSponsorForClubs();
+        
+        $startDate;
+        $endDate;
+        
+        $this->schoolStart = new \DateTime($this->container->getParameter("schoolYearStart"));
+        $this->q1End = new \DateTime($this->container->getParameter("q1End"));
+        $this->q2End = new \DateTime($this->container->getParameter("q2End"));
+        $this->q3End = new \DateTime($this->container->getParameter("q3End"));
+        $this->q4End = new \DateTime($this->container->getParameter("q4End"));
+        
+        if ($quarter == 0){
+            
+        }
+        else if($quarter == 1){
+            $startDate = $this->schoolStart;
+            $endDate = $this->q1End;
+        }
+        else if ($quarter == 2){
+            $startDate = $this->q1End;
+            $endDate = $this->q2End;
+        }
+        else if ($quarter == 3){
+            $startDate = $this->q2End;
+            $endDate = $this->q3End;
+        }
+        else if ($quarter == 4){
+            $startDate = $this->q3End;
+            $endDate = $this->q4End;
+        }
         
         foreach ($clubs as $club){
             foreach($club->getMembers() as $member){
@@ -21,14 +57,40 @@ class ClubController extends Controller
                 $records = $member->getRecords();
                 foreach($records as $record){
                     if ($record->getApprovalStatus() > 0){
-                        $approvedHours += $record->getNumHours();
+                        if($quarter == 0){
+                            $approvedHours += $record->getNumHours();
+                        }
+                        else if ($record->getDateTo() <= $endDate && $record->getDateTo() >= $startDate){
+                            $approvedHours += $record->getNumHours();
+                        }
                    }
                 }
                 $member->setApprovedHours($approvedHours);
             }
+            
+            if ($export == true){
+                $response = new StreamedResponse();
+                $response->setCallback(function() use ($club){
+                    $handle = fopen('php://output', 'w+');
+                    
+                    fputcsv($handle, array('Last Name', 'First Name', 'Hours'),',');
+                    
+                    foreach($club->getMembers() as $member){
+                        fputcsv($handle,array($member->getLastName(), $member->getFirstName(), $member->getApprovedHours()),',');
+                    }
+                    
+                    fclose($handle);
+                });
+                
+                $response->setStatusCode(200);
+                $response->headers->set('Content-Type', 'text/csv; charset=utf-8');
+                $response->headers->set('Content-Disposition','attachment; filename="export.csv"');
+
+                return $response;
+            }
         }
         
-        return $this->render("MaclayServiceBundle:Club:manage.html.twig", array("clubs" => $clubs, "error" => ""));
+        return $this->render("MaclayServiceBundle:Club:manage.html.twig", array("clubs" => $clubs, "error" => "", "quarter" => $quarter));
     }
     
     public function addClubMembersAction(){
